97a1ee2a244f1c3bcdde14d1b91dadd41be948d8
Chelsea S <csnider91@live.com>
2023-03-08 22:58:34 -0500

    added button styling

---
+++ frontend/static/src/components/Auth/LoginForm.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import Container from "react-bootstrap/esm/Container";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "../Styles/RegistrationStyles.css";
+import Cookies from "js-cookie";
+import { IconEye } from "@tabler/icons-react";
+
+function Login() {
+   return (
+      <Container>
+         <Form></Form>
+      </Container>
+   );
+}
+
+export default Login;
