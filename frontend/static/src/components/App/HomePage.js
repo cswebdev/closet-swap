d41e217d2fc62c2bfdba3df242fea873a0da0573
Chelsea S <csnider91@live.com>
2023-03-15 00:10:08 -0400

    minor update in color scheme and updated homepage with a hero-container and a graphic

--- frontend/static/src/components/App/HomePage.js
+++ frontend/static/src/components/App/HomePage.js
@@ -9,7 +9,24 @@ function HomePage() {
    return (
       <Container id="container-homepage">
          <Container id="hero-container">
-            <h1>Welcome to Closet Swap!</h1>
+            <div className="hero-container">
+               <img
+                  src="https://img.freepik.com/free-vector/hand-drawn-flat-lgbt-pride-day-background_23-2149393281.jpg?w=2000&t=st=1678852239~exp=1678852839~hmac=bbd796ae22848aeca96681fd30b5a51630ec607fbe091e2f63f6b4818b94c1a5"
+                  alt="Diverse community"
+                  className="hero-image"
+               />
+               <div className="hero-content">
+                  <h1 className="hero-title">
+                     Welcome to our diverse community
+                  </h1>
+                  <p className="hero-text">
+                     We strive to create an inclusive environment for everyone.
+                     Join us and be a part of our mission to celebrate diversity
+                     and promote equality.
+                  </p>
+                  <button className="hero-button">Join us</button>
+               </div>
+            </div>
          </Container>
       </Container>
    );
