3d8e3833daa04afd974d3b5389dc2095c7fe119d
Chelsea S <csnider91@live.com>
2023-03-20 23:32:59 -0400

    update in styling for user profile

--- frontend/static/src/components/Profile/ProfileForm.js
+++ frontend/static/src/components/Profile/ProfileForm.js
@@ -122,113 +122,127 @@ function ProfileForm() {
 
    const userClosetHTML = userCloset.map((item) => {
       return (
-         <Card className="col-8 col-md-4 col-lg-3 col-xl-2 d-flex w-">
-            <div className="p-0 m-0 g-0 overflow-hidden">
-               <Card.Img variant="top" src={item.image} className="CardImg " />
-            </div>
-            <Card.Body className="d-flex flex-column justify-content-center align-items-center overflow-hidden m-0 p-0">
-               <Card.Title className="p-1 m-1 text-center">
-                  {item.title}
-               </Card.Title>
-            </Card.Body>
-         </Card>
+         <div className="col-8 col-md-4 col-lg-3 col-xl-2 d-flex">
+            <Card className="">
+               <div className="">
+                  <Card.Img
+                     variant="top"
+                     src={item.image}
+                     className="CardImg "
+                  />
+               </div>
+               <Card.Body className="d-flex flex-column justify-content-center align-items-center overflow-hidden m-0 p-0">
+                  <Card.Title className="p-1 m-1 text-center">
+                     {item.title}
+                  </Card.Title>
+               </Card.Body>
+            </Card>
+         </div>
       );
    });
 
    return (
-      <Container id="profile-page" className="bg-white">
-         <Container id="container-profile" className="d-flex pt-5">
-            <Form
-               onSubmit={handleSubmit}
-               className="m-0 p-0 g-0"
-               id="profile-form"
-            >
-               <Container
-                  id="container-avatar"
-                  className=" text-center  m-0 p-0"
-               >
-                  <Container id="profile-avatar-container">
-                     <div id="user-header" className="d-flex text-center">
-                        <h1 className="text-center">{activeUser.username}</h1>
-                        <IconFlagFilled id="report" className="ms-1 mt-2" />
-                     </div>
-                     {preview && (
-                        <img src={preview} alt="" id="profile-avatar-image" />
-                     )}
-                     {!preview && (
-                        <img
-                           src={userProfile.avatar}
-                           alt=""
-                           id="profile-avatar-image"
-                        />
-                     )}
-                  </Container>
-                  <div className="d-flex p-0">
-                     <div className="d-flex  m-auto">
-                        <Form.Label htmlFor="item_image"></Form.Label>
-                        <Form.Control
-                           type="file"
-                           id="item_image"
-                           name="item_image"
-                           accept="image/*"
-                           className="form-control m-auto"
-                           onChange={handleImageInput}
-                        ></Form.Control>
-                     </div>
-                  </div>
-               </Container>
-               <Button
-                  variant="outline-primary"
-                  type="submit"
+      <>
+         <Container id="profile-page" className="bg-white">
+            <Container id="container-profile" className="d-flex pt-5">
+               <Form
                   onSubmit={handleSubmit}
+                  className="m-0 p-0 g-0"
+                  id="profile-form"
                >
-                  Save
-               </Button>
-            </Form>
-            <Form onSubmit={handleDisplayNamesInput} className="m-0 p-0 g-0">
-               <Container id="container-userinfo">
-                  <Row className="text-center">
-                     {/* <h1>User Info Goes Here</h1> */}
-                  </Row>
-                  <Row>
-                     <Form.Group className="mb-3">
-                        <Form.Label htmlFor="displayname"></Form.Label>
-                        <div className="input-group" id="displayname">
-                           <input
-                              className=" form-control"
-                              type="text"
-                              id="displayname"
-                              name="displayname"
-                              placeholder="Display Name"
-                              value={user.display_name}
-                              onChange={handleDisplayNamesInput}
-                           ></input>
+                  <Container
+                     id="container-avatar"
+                     className=" text-center  m-0 p-0"
+                  >
+                     <Container id="profile-avatar-container">
+                        <div id="user-header" className="d-flex text-center">
+                           <h1 className="text-center">
+                              {activeUser.username}
+                           </h1>
+                           <IconFlagFilled id="report" className="ms-1 mt-2" />
                         </div>
-                     </Form.Group>
-                  </Row>
-                  <Row className=" bg-info">
-                     <Col>
-                        <Row id="username">
-                           UserName: {userProfile.display_name}
-                        </Row>
-                        <Row>Gender: {userProfile.gender}</Row>
-                        <Row>City: {userProfile.city}</Row>
-                        <Row>State: {userProfile.state}</Row>
-                     </Col>
-                  </Row>
-               </Container>
-               <Container id="update-userinfo">
-                  <Row className="text-center"></Row>
-               </Container>
-            </Form>
+                        {preview && (
+                           <img
+                              src={preview}
+                              alt=""
+                              id="profile-avatar-image"
+                           />
+                        )}
+                        {!preview && (
+                           <img
+                              src={userProfile.avatar}
+                              alt=""
+                              id="profile-avatar-image"
+                           />
+                        )}
+                     </Container>
+                     <div className="d-flex p-0">
+                        <div className="d-flex  m-auto">
+                           <Form.Label htmlFor="item_image"></Form.Label>
+                           <Form.Control
+                              type="file"
+                              id="item_image"
+                              name="item_image"
+                              accept="image/*"
+                              className="form-control m-auto"
+                              onChange={handleImageInput}
+                           ></Form.Control>
+                        </div>
+                     </div>
+                  </Container>
+                  <Button
+                     variant="outline-primary"
+                     type="submit"
+                     onSubmit={handleSubmit}
+                  >
+                     Save
+                  </Button>
+               </Form>
+               <Form onSubmit={handleDisplayNamesInput} className="m-0 p-0 g-0">
+                  <Container id="container-userinfo">
+                     <Row className="text-center">
+                        {/* <h1>User Info Goes Here</h1> */}
+                     </Row>
+                     <Row>
+                        <Form.Group className="mb-3">
+                           <Form.Label htmlFor="displayname"></Form.Label>
+                           <div className="input-group" id="displayname">
+                              <input
+                                 className=" form-control"
+                                 type="text"
+                                 id="displayname"
+                                 name="displayname"
+                                 placeholder="Display Name"
+                                 value={user.display_name}
+                                 onChange={handleDisplayNamesInput}
+                              ></input>
+                           </div>
+                        </Form.Group>
+                     </Row>
+                     <Row className=" bg-info">
+                        <Col>
+                           <Row id="username">
+                              UserName: {userProfile.display_name}
+                           </Row>
+                           <Row>Gender: {userProfile.gender}</Row>
+                           <Row>City: {userProfile.city}</Row>
+                           <Row>State: {userProfile.state}</Row>
+                        </Col>
+                     </Row>
+                  </Container>
+                  <Container id="update-userinfo">
+                     <Row className="text-center"></Row>
+                  </Container>
+               </Form>
+            </Container>
+            <Container id="container-closet">
+               <div className="text-center mt-5">
+                  <h1>User Clothes</h1>
+                  <div>{userClosetHTML}</div>
+               </div>
+            </Container>
          </Container>
-         <Container id="container-closet">
-            <div className="text-center mt-5">
-               <h1>User Clothes</h1>
-               <div>{userClosetHTML}</div>
-            </div>
-         </Container>
-      </Container>
+      </>
    );
 }
 
